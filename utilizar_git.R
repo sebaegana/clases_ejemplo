9d516ec1e6b4c491a97382f09d35b6f10e849b59
sebagana <sebaegana@gmail.com>
2025-07-10 19:41:09 -0400

    Subida de archivos

---
+++ utilizar_git.R
@@ -0,0 +1,13 @@
+install.packages(c("usethis", "renv", "gh"))
+
+usethis::use_git()
+
+usethis::use_github()
+
+usethis::create_github_token()
+
+usethis::edit_r_environ()
+
+gitcreds::gitcreds_set()
+
+usethis::git_sitrep()
